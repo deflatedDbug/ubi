817f5edbee9e7a153d27168495b28cadf73c0089
Subin <subinlebow2@gmail.com>
2025-03-02 03:25:13 -0500

    initial push

---
+++ app/component/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { DashboardLayout } from '@toolpad/core/DashboardLayout'
+
+export default function Dashboard () {
+    return(
+        
+        <DashboardLayout> 
+            <h1>
+                Hello
+            </h1>
+        </DashboardLayout>
+    )
+}
